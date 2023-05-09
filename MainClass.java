774b0f470fea10de631228dba8b4e58e875a1d7b
kjvlbu <114408460+kjvlbu@users.noreply.github.com>
2023-05-09 01:07:29 +0100

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,24 +3,27 @@ package oop;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
 
+
 public class MainClass{
 		
 		GraphicsSystem gfx = new GraphicsSystem();
 		
         public static void main(String[] args) {
                 
-        	new MainClass();
-        	new GraphicsSystem();
+        		new MainClass();
+        		new GraphicsSystem();
         }
 
         public MainClass() {
                 
-        	JFrame MainFrame = new JFrame("LBU Graphics Application");                
+        		JFrame MainFrame = new JFrame("LBU Graphics Application");                
                 MainFrame.setLayout(new FlowLayout()); 
                 MainFrame.add(gfx);                                    
                 MainFrame.pack();                                               
                 MainFrame.setVisible(true);  
                 MainFrame.setResizable(false);
                 
+                gfx.penDown();
+                // gfx.about();
         } 
 }
